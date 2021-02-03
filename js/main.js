a0b3457ca5afd9f7b55faa31115ed29b983f0a56
delafossa <gentile08111@gmail.com>
2021-02-03 17:25:47 +0400

    asdas

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
-(function() {
+// === menuItem ===
 
-   var hamburger = {
-     navToggle: document.querySelector('.nav-toggle'),
-     nav: document.querySelector('nav'),
- 
-     doToggle: function(e) {
-       e.preventDefault();
-       this.navToggle.classList.toggle('expanded');
-       this.nav.classList.toggle('expanded');
-     }
-   };
- 
-   hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-   hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+const menuBtn = document.querySelector('.menu-btn');
+const mainMenu = document.querySelector('.main-menu');
+
+// --- menuItem toggle ---
+
+menuBtn.addEventListener('click', () => {
+  
+  menuBtn.classList.toggle('open');
+  mainMenu.classList.toggle('open');
+
+})
 
 
- 
- }());
+
+function enableSubmitBlack() {
+  document.getElementById("submit-black").disabled = false;
+  }
+
+  function enableSubmitWhite() {
+    document.getElementById("submit-white").disabled = false;
+    }
+
+
+
+
+
+
+
+
+
